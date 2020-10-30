9a51ba5e266d9271815e42e68435dc501aec0820
duduws <64056323+duduws@users.noreply.github.com>
2020-10-29 22:09:26 -0300

    Update rndstatus.py

--- rndstatus/rndstatus.py
+++ rndstatus/rndstatus.py
@@ -177,7 +177,7 @@ class RndStatus(commands.Cog):
             clean_prefix = pattern.sub(f"@{me.name}", prefix[0])
             total_users = len(self.bot.users)
             servers = str(len(self.bot.guilds))
-            botstatus = f"{clean_prefix}help | {total_users} users | {servers} servers"
+            botstatus = {total_users} Membros"
             if (current_game != str(botstatus)) or current_game is None:
                 if _type == 1:
                     await self.bot.change_presence(activity=discord.Streaming(name=botstatus, url=url))
